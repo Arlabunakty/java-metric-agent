ee9f0f8069134ae3ab9f6a57a549fe6990e44709
Arlabunakty <durniev.denys@gmail.com>
2020-09-25 16:31:12 +0300

    Add sample Spring Boot project with two pages for testing agent integration:  1) welcome page;  2) user welcome page with random response delay.

---
+++ webapp/src/main/java/ua/arlabunakty/DefaulController.java
@@ -0,0 +1,45 @@
+package ua.arlabunakty;
+
+import java.util.Random;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class DefaulController {
+
+    private final String appName;
+
+    public DefaulController(@Value("${spring.application.name}") String appName) {
+        this.appName = appName;
+    }
+
+    @GetMapping("/")
+    public String homePage(Model model) {
+        model.addAttribute("appName", appName);
+        return "home";
+    }
+
+    /**
+     * Return greeting template for user provided name with random delay.
+     *
+     * @param name  - user name
+     * @param model - template model attributes holder
+     * @return template name
+     * @throws InterruptedException when during sleep period thread was interrupted.
+     */
+    @GetMapping("/hello")
+    public String homePageWithUserGreeting(
+            @RequestParam(name = "name", required = false, defaultValue = "")
+                    String name, Model model) throws InterruptedException {
+        Thread.sleep(new Random().nextInt(5000));
+
+        model.addAttribute("appName", appName);
+        model.addAttribute("name", name);
+
+        return "home";
+    }
+}
+
