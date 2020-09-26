ba547849c1a1c919d2fa05908e135279ffccd3ea
Arlabunakty <durniev.denys@gmail.com>
2020-09-26 21:03:13 +0300

    Polish integration code.

--- webapp/src/test/java/ua/arlabunakty/IndexControllerIntegrationTest.java
+++ webapp/src/test/java/ua/arlabunakty/IndexControllerIntegrationTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.net.URL;
+import java.util.Collections;
+import java.util.Optional;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.junit.jupiter.api.BeforeEach;
@@ -20,7 +22,8 @@ import org.springframework.http.ResponseEntity;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class IndexControllerIntegrationTest {
 
-    public static final String X_METRIC_TRACE_ID_HEADER_NAME = "X-Metric-Trace-Id";
+    private static final String X_METRIC_TRACE_ID_HEADER_NAME = "X-Metric-Trace-Id";
+
     @LocalServerPort
     private int port;
 
@@ -35,8 +38,17 @@ class IndexControllerIntegrationTest {
     }
 
     @Test
-    void indexPage() {
-        ResponseEntity<String> response = template.getForEntity(base.toString(), String.class);
+    void shouldJavaAgentInterceptCallAndRegisterMetricForIndexPage() {
+        assertMetricsForEndpoint(base.toString());
+    }
+
+    @Test
+    void shouldJavaAgentInterceptCallAndRegisterMetricForIndexPageWithUserGreeting() {
+        assertMetricsForEndpoint(base.toString() + "hello?name=username");
+    }
+
+    private void assertMetricsForEndpoint(String url) {
+        ResponseEntity<String> response = template.getForEntity(url, String.class);
 
         assertTrue(response.getHeaders()
                 .containsKey(X_METRIC_TRACE_ID_HEADER_NAME), "response should have trace header");
@@ -44,14 +56,17 @@ class IndexControllerIntegrationTest {
         String traceId = getTraceId(response);
         MetricsByTraceId metricsByTraceId = getMetricsByTraceId(traceId);
 
-        assertEquals(response.getBody().length(), metricsByTraceId.getResponseBodyLength());
+        assertEquals(getResponseBodyLength(response), metricsByTraceId.getResponseBodyLength());
         assertTrue(metricsByTraceId.getRequestOperationTime() > 0);
     }
 
     private String getTraceId(ResponseEntity<String> response) {
-        return response.getHeaders()
-                .get(X_METRIC_TRACE_ID_HEADER_NAME)
-                .get(0);
+        return Optional.ofNullable(response.getHeaders()
+                .get(X_METRIC_TRACE_ID_HEADER_NAME))
+                .orElse(Collections.emptyList())
+                .stream()
+                .findFirst()
+                .orElse("");
     }
 
     private MetricsByTraceId getMetricsByTraceId(String traceId) {
@@ -67,20 +82,10 @@ class IndexControllerIntegrationTest {
         return new MetricsByTraceId(requestOperationTime, responseBodyLength);
     }
 
-    @Test
-    void indexPageWithUserGreeting() {
-        String url = base.toString() + "hello?name=username";
-
-        ResponseEntity<String> response = template.getForEntity(url, String.class);
-
-        assertTrue(response.getHeaders()
-                .containsKey(X_METRIC_TRACE_ID_HEADER_NAME), "response should have trace header");
-
-        String traceId = getTraceId(response);
-        MetricsByTraceId metricsByTraceId = getMetricsByTraceId(traceId);
-
-        assertEquals(response.getBody().length(), metricsByTraceId.getResponseBodyLength());
-        assertTrue(metricsByTraceId.getRequestOperationTime() > 0);
+    private long getResponseBodyLength(ResponseEntity<String> response) {
+        return (long) Optional.ofNullable(response.getBody())
+                .map(String::length)
+                .orElse(0);
     }
 
     private static class MetricsByTraceId {
